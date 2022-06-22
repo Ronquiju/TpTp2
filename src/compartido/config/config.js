afed2291a20445f1a7cacd5bffd877fb50b01c7b
Ronquiju <ronquiju@gmail.com>
2022-06-22 18:35:31 -0300

    algo de login

--- src/compartido/config/config.js
+++ src/compartido/config/config.js
@@ -1,2 +1,2 @@
-export const MODO_PERSISTENCIA = 'BDD'
+export const MODO_PERSISTENCIA = 'ARCHIVO'
 export const CNX_STR = "mongodb://root:123456@localhost:27017?retryWrites=true&writeConcern=majority"
